5a20c8e0b050cc1d83612a65fbf038e8314695e0
Michael.Makukha <m.makukha@gmail.com>
2025-05-24 21:51:09 +0300

    Fix lint errors

--- tests/test_symlink.py
+++ tests/test_symlink.py
@@ -1,17 +1,17 @@
 from dirlay import Dir
-import pytest
 
-from pydantic_file_secrets import SettingsConfigDict, SettingsError
+from pydantic_file_secrets import SettingsConfigDict
 from tests.sample import AppSettings
 
 
 def test_subdir(monkeypatch, tmp_path):
     monkeypatch.setenv('DB__USER', 'user')
+    # fmt: off
     secrets = Dir() | {
         'app_key': 'secret1',
         'db_random/passwd': 'secret2',  # file in subdir that is not directly referenced in our settings
     }
-
+    # fmt: on
 
     class Settings(AppSettings):
         model_config = SettingsConfigDict(
@@ -20,28 +20,11 @@ def test_subdir(monkeypatch, tmp_path):
             secrets_nested_subdir=True,
         )
 
-
-
     with secrets.mktree(tmp_path):
-
         # create a symlink to the match our settings
         tmp_path.joinpath('db').symlink_to(tmp_path.joinpath('db_random'))
 
         assert Settings().model_dump() == {
             'app_key': 'secret1',
-            'db': {'user': 'user', 'passwd': 'secret2'}
+            'db': {'user': 'user', 'passwd': 'secret2'},
         }
-
-
-def test_invalid_options(tmp_path):
-    class Settings(AppSettings):
-        model_config = SettingsConfigDict(
-            secrets_dir=tmp_path,
-            env_nested_delimiter='__',
-            secrets_nested_subdir=True,
-            secrets_nested_delimiter='__',
-        )
-
-
-    with pytest.raises(SettingsError, match='mutually exclusive'):
-        Settings()
